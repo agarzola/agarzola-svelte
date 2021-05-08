a8f68859c7adcecfd50f9971d9b49a1d7522693b
Alfonso Gómez-Arzola <alfonso@chromatichq.com>
2021-05-08 07:44:34 -0400

    Remove vite config altogether

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,4 @@
 import netlify from '@sveltejs/adapter-netlify';
-import { readFileSync } from 'fs';
-const pkg = JSON.parse( readFileSync(new URL('package.json', import.meta.url), 'utf8') );
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -8,12 +6,6 @@ const config = {
 		adapter: netlify(),
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-		// Vite configuration.
-		vite: {
-			optimizeDeps: {
-				include: Object.keys(pkg.dependencies || {}),
-			},
-		},
 	},
 };
 
