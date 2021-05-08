b8d64e2bf21c5e2a1122d9aebbf861d4ab21034a
Alfonso Gómez-Arzola <alfonso@chromatichq.com>
2021-05-08 07:18:38 -0400

    Set luxon as a regular dependency & optimize it

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,20 @@
 import netlify from '@sveltejs/adapter-netlify';
+import { readFileSync } from 'fs';
+const pkg = JSON.parse( readFileSync(new URL('package.json', import.meta.url), 'utf8') );
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// By default, `npm run build` will create a standard Node app.
-		// You can create optimized builds for different platforms by
-		// specifying a different adapter
 		adapter: netlify(),
-
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte',
-
-		vite: {}
-	}
+		// Vite configuration.
+		vite: {
+			optimizeDeps: {
+				include: Object.keys(pkg.dependencies || {}),
+			},
+		},
+	},
 };
 
 export default config;
