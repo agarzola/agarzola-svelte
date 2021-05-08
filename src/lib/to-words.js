65d7e6fe2a82421a5b243ba66b9743c13fce4b12
Alfonso Gómez-Arzola <alfonso@chromatichq.com>
2021-05-08 06:51:21 -0400

    Create temporary to-words replacement lib

---
+++ src/lib/to-words.js
@@ -0,0 +1,32 @@
+// Construct a contrived version of to-words while I sort out the package
+// inclusion issue.
+const toWords = {
+	convert(number) {
+		switch (number) {
+			case 0:
+				return 'zero';
+			case 1:
+				return 'one';
+			case 2:
+				return 'one';
+			case 3:
+				return 'one';
+			case 4:
+				return 'one';
+			case 5:
+				return 'one';
+			case 6:
+				return 'one';
+			case 7:
+				return 'one';
+			case 8:
+				return 'one';
+			case 9:
+				return 'one';
+			case 10:
+				return 'one';
+		}
+	},
+};
+
+export default toWords;
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,6 +5,7 @@
 	export const prerender = true;
 	export const router = false;
 
+	import toWords from '$lib/to-words';
 	import Prismic from '@prismicio/client';
 	const Client = Prismic.client('https://agarzola.cdn.prismic.io/api/v2');
 	import PrismicDom from 'prismic-dom';
@@ -14,7 +15,7 @@
 			[ Prismic.Predicates.at('document.type', 'journal_entry') ],
 			{ pageSize: 15, page: 1, orderings: '[my.journal_entry.date desc]' }
 		);
-		const total_journal_entries = entries.results.length;
+		const total_journal_entries = toWords.convert(entries.results.length).toLowerCase();
 		return { props: { total_journal_entries } };
 	}
 </script>
@@ -33,7 +34,7 @@
 	<h1>Hello. I’m&nbsp;Alfonso.</h1>
 	<img src='/portrait-small.jpg' srcset='/portrait-small.jpg 640w, /portrait-large.jpg 1080w' alt='A black and white photo of me taking a picture of myself with my smartphone on a mirror with an ornate frame. A modern pendant lamp is visible in the top right corner, and the edges of a fiddle leaf fig tree near the lower right corner.' />
 	<p>
-	This is where I put the things I’m interested in. You should check out <a class='highlighted' href='/journal'>my journal</a>, where I have rambled exactly { total_journal_entries } time{ total_journal_entries !== 1 ? 's' : '' } —with varying degrees of eloquence— about something or other.
+	This is where I put the things I’m interested in. You should check out <a class='highlighted' href='/journal'>my journal</a>, where I have rambled exactly { total_journal_entries } time{ total_journal_entries !== 'one' ? 's' : '' } —with varying degrees of eloquence— about something or other.
 	</p>
 	<p>
 	You can also find me on <a class='highlighted' rel='external' href='https://www.linkedin.com/in/agarzola/'>LinkedIn</a>, <a class='highlighted' rel='external' href='https://instagram.com/agarzola'>Instagram</a>, and —sadly— the absolute pit of despair that is <a class='highlighted' rel='external' href='https://twitter.com/agarzola'>Twitter</a>. You should follow me on none of&nbsp;these.
