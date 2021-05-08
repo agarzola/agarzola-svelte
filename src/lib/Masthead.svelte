5b2f2e3e30cdeb8017dfed117b6077525ab53e6c
Alfonso Gómez-Arzola <alfonso@chromatichq.com>
2021-05-08 13:35:51 -0400

    Create Masthead and JournalEntryHeading components (#2)

---
+++ src/lib/Masthead.svelte
@@ -0,0 +1,3 @@
+<aside class='masthead-notice'>
+	<slot></slot>
+</aside>
--- src/routes/journal/index.svelte
+++ src/routes/journal/index.svelte
@@ -22,30 +22,23 @@
 <script>
 	export let entries;
 	import entryDateTime from '$lib/entry-date-time';
+	import JournalEntryHeading from '$lib/JournalEntryHeading.svelte';
+	import Masthead from '$lib/Masthead.svelte';
 </script>
 
-<style>
-	time {
-		font-size: 0.8rem;
-		color: var(--muted);
-	}
-</style>
-
-<aside class='masthead-notice'>
+<Masthead>
 	<p>Hello. I’m Alfonso and this is my <span class="defined-term" title="That’s a fancy word for “diary” or, more recently, “blog”.">journal</span>. Want to learn more about me? <a class='highlighted' href="/">Visit&nbsp;my&nbsp;homepage.</a></p>
-</aside>
+</Masthead>
 
 <main>
 	<h1>Journal.</h1>
 	{#each entries.results as entry}
 	<div class='journal-entry'>
-		<a href='/journal/{ entry.slugs[0] }'>
-			<h2>{ PrismicDom.RichText.asText(entry.data.title) }</h2>
-		</a>
-		<time datetime='{ entry.last_publication_date }'>
-			{ entryDateTime(entry.last_publication_date).date } at
-			{ entryDateTime(entry.last_publication_date).time }
-		</time>
+		<JournalEntryHeading
+			link={ true }
+			slug={ entry.slugs[0] }
+			timestamp={ entry.last_publication_date }
+			title={ PrismicDom.RichText.asText(entry.data.title) } />
 		{@html PrismicDom.RichText.asHtml(entry.data.summary)}
 	</div>
 	{/each}
